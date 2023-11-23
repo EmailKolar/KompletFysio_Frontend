d64de902c63a15d124b2dbe559a4d824db64ee60
Nademtis <Mathias.99.madsen@gmail.com>
2023-11-23 15:47:52 +0100

    added files

---
+++ project/javascript/customerBookAppointment.js
@@ -0,0 +1,9 @@
+// const myModal = new bootstrap.Modal(document.getElementById('myModal'), {
+//     keyboard: false
+// });
+
+let button = document.getElementById("openModal");
+
+button.addEventListener("click", () => {
+    // myModal.toggle();
+});
