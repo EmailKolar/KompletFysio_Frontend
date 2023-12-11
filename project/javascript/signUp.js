f58a2de535357be7b8c34c637220adc2dc55701b
EmailKolar <Emilaalund.nyboder@gmail.com>
2023-12-11 09:16:47 +0100

    sign up working

---
+++ project/javascript/signUp.js
@@ -0,0 +1,60 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+
+
+
+
+const fetchUrl = "http://localhost:8080/saveCustomer"
+
+
+document.getElementById("signupForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let fname = document.getElementById("fnameField").value;
+    let lname = document.getElementById("lnameField").value;
+    let dob = document.getElementById("dobField").value;
+    let cpr = document.getElementById("cprField").value;
+    let address = document.getElementById("addressField").value;
+    let post = document.getElementById("post").value;
+    let city = document.getElementById("cityField").value;
+    let username = document.getElementById("usernameFieldSignup").value;
+    let password = document.getElementById("passwordFieldSignup").value;
+
+
+
+   let  bodylist = {
+       customerId : 1,
+       firstName : fname,
+       lastName : lname,
+       dateOfBirth : dob,
+       cpr : cpr,
+       address : address,
+       zipCode : post,
+       city : city,
+       username : username,
+       password : password
+   }
+
+   let body = JSON.stringify(bodylist)
+
+    console.log(body)
+
+
+    fetch(fetchUrl,{
+        method: "POST",
+        body : body,
+        headers:{
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+
+})
+
+})
